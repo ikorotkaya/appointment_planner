a1b1eea3a6762a6e2902f1717a292ee426a9b9ba
ikorotkaya <irinkakorotkaya@gmail.com>
2023-02-04 22:48:41 +0100

    Add contacts and appointments states to App component, set callback functions and pass props to child components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
+
+  const addNewContact = (name, phone, email) => {
+    setContacts([...contacts, {name, phone, email}]);
+  }
+
+  const addNewAppointment = (title, contact, date, time) => {
+    setAppointments([...appointments, {title, contact, date, time}]);
+  }
 
   const ROUTES = {
     CONTACTS: "/contacts",
     APPOINTMENTS: "/appointments",
   };
-
-  /*
-  Implement functions to add data to
-  contacts and appointments
-  */
 
   return (
     <>
@@ -37,11 +38,11 @@ function App() {
           </Route>
           <Route path={ROUTES.CONTACTS}>
              {/* Add props to ContactsPage */}
-            <ContactsPage />
+            <ContactsPage contacts={contacts} addNewContact={addNewContact}/>
           </Route>
           <Route path={ROUTES.APPOINTMENTS}>
             {/* Add props to AppointmentsPage */}
-            <AppointmentsPage />
+            <AppointmentsPage appointments={appointments} contacts={contacts} addNewAppointment={addNewAppointment}/>
           </Route>
         </Switch>
       </main>
