6cf2dfb901c5d6a69d13fba5c894e5cb571f6751
ikorotkaya <irinkakorotkaya@gmail.com>
2023-02-05 19:26:15 +0100

    Create AppointmentForm as a stateless component to render a web form to collect the appointment info

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {ContactPicker} from "../../components/contactPicker/ContactPicker"
 
 export const AppointmentForm = ({
   contacts,
@@ -20,6 +21,32 @@ export const AppointmentForm = ({
   };
 
   return (
-    AppointmentForm
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        id="title"
+        required />
+      <input
+        type="date"
+        min={getTodayString}
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+        id="date"
+        required />
+      <input
+        type="time"
+        value={time}
+        onChange={(e) => setTime(e.target.value)}
+        id="time"
+        required />
+      <ContactPicker 
+        contacts={contacts}
+        onChange={(e) => setContact(e.target.value)} />
+      <button
+        type="submit"
+        onChange={handleSubmit}>Submit</button>
+    </form>
   );
 };
